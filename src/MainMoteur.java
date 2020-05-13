9f9efc41c96c9c70c1b1f3e172a5f2fb4f9df797
Oussenekan <mael.oussenekan@insa-lyon.fr>
2020-05-13 18:11:36 +0200

    J'ai rajouter un début de classe moteur

---
+++ src/MainMoteur.java
@@ -0,0 +1,9 @@
+public class MainMoteur {
+    public static void main(String[] args) {
+    System.out.println("Yo");
+    Moteur trackeur = new Moteur();
+    System.out.println(trackeur);
+    trackeur.setMoteur_bas(90);
+    System.out.println(trackeur);
+    }
+}
