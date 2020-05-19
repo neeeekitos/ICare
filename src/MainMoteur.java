55b3002325185bd8078bba294d673f40b02d7e86
Oussenekan <mael.oussenekan@insa-lyon.fr>
2020-05-19 11:45:20 +0200

    Il n y a que la classe Moteur_Seul vraiment interressant

--- src/MainMoteur.java
+++
@@ -1,9 +0,0 @@
-public class MainMoteur {
-    public static void main(String[] args) {
-    System.out.println("Yo");
-    Moteur trackeur = new Moteur();
-    System.out.println(trackeur);
-    trackeur.setMoteur_bas(90);
-    System.out.println(trackeur);
-    }
-}
