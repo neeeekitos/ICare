bc702889a5fdce487244efe025b37779c62e4418
neeeekitos <nikita.a.terekhov@gmail.com>
2020-05-21 20:17:38 +0200

    new classes

---
+++ LWJGL/src/DummyGame.java
@@ -0,0 +1,95 @@
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Paths;
+
+import static org.lwjgl.glfw.GLFW.*;
+
+public class DummyGame implements IGameLogic {
+
+    private static final float MOUSE_SENSITIVITY = 0.2f;
+
+    private final Vector3f cameraInc;
+
+    private final Renderer renderer;
+
+    private final Camera camera;
+
+    private GameItem[] gameItems;
+
+    private static final float CAMERA_POS_STEP = 0.05f;
+
+    public DummyGame() {
+        renderer = new Renderer();
+        camera = new Camera();
+        cameraInc = new Vector3f();
+    }
+
+    @Override
+    public void init(Window window) throws Exception {
+        renderer.init(window);
+
+        //Mesh mesh = OBJLoader.loadMesh("/models/bunny.obj");
+        Mesh mesh = OBJLoader.loadMesh("/models/assemblage.obj");
+
+        //generate absolute path to open texture
+        URL res = getClass().getClassLoader().getResource("pornhub.png");
+        File file = Paths.get(res.toURI()).toFile();
+        String absolutePath = file.getAbsolutePath();
+        Texture texture = new Texture(absolutePath);
+
+        mesh.setTexture(texture);
+        GameItem gameItem = new GameItem(mesh);
+        gameItem.setScale(0.5f);
+        gameItem.setPosition(0, 0, -2);
+        gameItems = new GameItem[]{gameItem};
+    }
+
+    @Override
+    public void input(Window window, MouseInput mouseInput) {
+        cameraInc.set(0, 0, 0);
+        if (window.isKeyPressed(GLFW_KEY_W)) {
+            cameraInc.z = -1;
+        } else if (window.isKeyPressed(GLFW_KEY_S)) {
+            cameraInc.z = 1;
+        }
+        if (window.isKeyPressed(GLFW_KEY_A)) {
+            cameraInc.x = -1;
+        } else if (window.isKeyPressed(GLFW_KEY_D)) {
+            cameraInc.x = 1;
+        }
+        if (window.isKeyPressed(GLFW_KEY_Z)) {
+            cameraInc.y = -1;
+        } else if (window.isKeyPressed(GLFW_KEY_X)) {
+            cameraInc.y = 1;
+        }
+    }
+
+    @Override
+    public void update(float interval, MouseInput mouseInput) {
+        // Update camera position
+        camera.movePosition(cameraInc.x * CAMERA_POS_STEP, cameraInc.y * CAMERA_POS_STEP, cameraInc.z * CAMERA_POS_STEP);
+
+        // Update camera based on mouse            
+        if (mouseInput.isRightButtonPressed() || mouseInput.isLeftButtonPressed()) {
+            Vector2f rotVec = mouseInput.getDisplVec();
+            camera.moveRotation(rotVec.x * MOUSE_SENSITIVITY, rotVec.y * MOUSE_SENSITIVITY, 0);
+        }
+    }
+
+    @Override
+    public void render(Window window) {
+        renderer.render(window, camera, gameItems);
+    }
+
+    @Override
+    public void cleanup() {
+        renderer.cleanup();
+        for (GameItem gameItem : gameItems) {
+            gameItem.getMesh().cleanUp();
+        }
+    }
+
+}
---
+++ Models/src/ReadCSV.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadCSV {
+    private boolean pm;
+    private double[][] data; // temps | zenith | azimuth
+    private int i;
+    private int taille;
+    private String fileName;
+    private File file;
+
+    /***
+    * Constructeur de la class ReadCSV
+     ***/
+    public ReadCSV(){
+        fileName = "doc/solarOrbit.csv";
+        file = new File(fileName);
+        pm=false;
+        taille=96;
+        data = new double[3][taille];
+        i=0;
+    }
+
+    /***
+    * Méthode permettant de renvoyer un tableau contenant de renvoyer un tableau contenant les données intéressantes du document csv
+     ***/
+    public double[][] getData(){
+        try{
+            Scanner inputStream = new Scanner(file);
+            inputStream.next();
+            while(inputStream.hasNext()){
+                String donnee = inputStream.next();
+                String [] values = donnee.split(";");
+                if(values.length>1){
+                    if(values[0].equals("0:0")){ // cette condition permet de commencer a enregistrer les données à partir de la première ligne de donnée
+                        pm=true;
+                    }
+                }
+
+                if(pm && i<taille) { // la condition "i<taille" permet de ne pas essayer d'enregistrer des données de cases vides
+                    data[0][i]=0.25*i;
+                    data[1][i]=Double.parseDouble(values[1]);
+                    data[2][i]=Double.parseDouble(values[2]);
+                    i++;
+                }
+            }
+            inputStream.close();
+
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        return data;
+    }
+}
