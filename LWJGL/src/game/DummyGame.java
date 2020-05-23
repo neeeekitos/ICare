abc42ef1ba23817886b70059d710df5b17fa5371
Oussenekan <mael.oussenekan@insa-lyon.fr>
2020-05-23 16:48:00 +0200

    Le soleil peut tourner partout et on peut le regler

--- LWJGL/src/game/DummyGame.java
+++ LWJGL/src/game/DummyGame.java
@@ -41,13 +41,17 @@ public class DummyGame implements IGameLogic {
     
     private float azimutSoleil;
 
-    private float zenithSoleil;
+    private float zenithSoleil;             //Correspond à un l'angle entre l'axe x et y est compris entre 0 et 360°
+
+    private float zenithSoleilpourAffichage; //Correspond à un l'angle entre le plan x0z et y est compris entre -90° et 90°
 
     public DummyGame() {
         renderer = new Renderer();
         camera = new Camera();
         cameraInc = new Vector3f(0.0f, 0.0f, 0.0f);
         zenithSoleil = 90;
+        zenithSoleilpourAffichage = zenithSoleil;
+        azimutSoleil = 0;
     }
 
     @Override
@@ -81,17 +85,17 @@ public class DummyGame implements IGameLogic {
         quadMesh.setMaterial(quadMaterial);
         GameItem quadGameItem = new GameItem(quadMesh);
         quadGameItem.setPosition(0, -1, 1);
-        quadGameItem.setScale(4f);
+        quadGameItem.setScale(2.5f);
 
         scene.setGameItems(new GameItem[]{cubeGameItem,SoleilGameItem, quadGameItem});
 
         // Setup Lights
         setupLights();
 
-        camera.getPosition().z = 5;
-        camera.getPosition().y = 2f;
+        camera.getPosition().z = 2;
+        camera.getPosition().y = 1;
         camera.getRotation().x = 30;
-        hud = new Hud("Light Angle:");
+        hud = new Hud("Azimut Angle:");
     }
 
     private void setupLights() {
@@ -109,14 +113,14 @@ public class DummyGame implements IGameLogic {
         directionalLight.setOrthoCords(-10.0f, 10.0f, -10.0f, 10.0f, -1.0f, 20.0f);
         sceneLight.setDirectionalLight(directionalLight);
 
-        // Point Light Représente le soleil
+        // Point Light Représentant le soleil
         PointLight [] source = new PointLight[2];
         float Point_lightIntensity = 0.8f;
-        Vector3f PointlightDirection_0 = new Vector3f(0, 0, -0.5f); // Soleil principal
+        Vector3f PointlightDirection_0 = new Vector3f(SoleilGameItem.getPosition().x,SoleilGameItem.getPosition().y,SoleilGameItem.getPosition().z); // Soleil principal
         PointLight.Attenuation AttenuationSoleil = new PointLight.Attenuation(0,0,0.7f);
         source [0] = new PointLight(new Vector3f(1,1,1),PointlightDirection_0,Point_lightIntensity);
         source [0].setAttenuation(AttenuationSoleil);
-        Vector3f PointlightDirection_1 = new Vector3f(0, -0.2f, 0.5f); // Soleil' éclaire la surface par en dessous sinon ombre
+        Vector3f PointlightDirection_1 = new Vector3f(0, -0.2f, 0.5f); // Soleil' éclaire la surface par en dessous sinon il y a de l'ombre sur le soleil
         source [1] = new PointLight(new Vector3f(1,1,1),PointlightDirection_1,0.6f);
         sceneLight.setPointLightList(source);
 
@@ -142,17 +146,17 @@ public class DummyGame implements IGameLogic {
         }
         if (window.isKeyPressed(GLFW_KEY_UP)) {
             zenithSoleil += 1;
-            ZenithSoleil(zenithSoleil);
+            angleSoleil(zenithSoleil,azimutSoleil);
         } else if (window.isKeyPressed(GLFW_KEY_DOWN)) {
             zenithSoleil -= 1;
-            ZenithSoleil(zenithSoleil);
+            angleSoleil(zenithSoleil,azimutSoleil);
         }
         if (window.isKeyPressed(GLFW_KEY_LEFT)) {
-            azimutSoleil -= 0.05f;
+            azimutSoleil -= 1;
+            angleSoleil(zenithSoleil,azimutSoleil);
         } else if (window.isKeyPressed(GLFW_KEY_RIGHT)) {
-            azimutSoleil += 0.05f;
-        } else {
-            azimutSoleil = 0;
+            azimutSoleil += 1;
+            angleSoleil(zenithSoleil,azimutSoleil);
         }
     }
 
@@ -194,15 +198,16 @@ public class DummyGame implements IGameLogic {
             this.scene.getSceneLight().getPointLightList(1).setIntensity((SoleilGameItem.getPosition().y +1)*0.4f+0.2f);   // l'intensité du soleil' oscille entre 0.6 et 0.2
             lightDirection.x = SoleilGameItem.getPosition().x;
             lightDirection.y = SoleilGameItem.getPosition().y +1;
-            lightDirection.z = 0.5f;
+            lightDirection.z = SoleilGameItem.getPosition().z ;
             PointlightDirection.x = SoleilGameItem.getPosition().x;
             PointlightDirection.y = SoleilGameItem.getPosition().y +1;
-            PointlightDirection.z = 0f;
+            PointlightDirection.z = SoleilGameItem.getPosition().z;
         }
-        this.scene.getSceneLight().getPointLightList(0).getAttenuation().setExponent(Math.abs(0.7f*(SoleilGameItem.getPosition().x)));
+        this.scene.getSceneLight().getPointLightList(0).getAttenuation().setExponent(0.75f*Math.abs((zenithSoleilpourAffichage/90)+1));
         this.scene.getSceneLight().getDirectionalLight().setIntensity(SoleilGameItem.getPosition().y +1);
         PointlightDirection.normalize();
         lightDirection.normalize();
+        hud.setStatusText("Azimut = " + azimutSoleil+" / zenith (angle) = "+ zenithSoleil+" / zenith (definition) = " + zenithSoleilpourAffichage);
 
     }
 
@@ -223,11 +228,32 @@ public class DummyGame implements IGameLogic {
         }
     }
 
-    public void ZenithSoleil( float angle ){
-        float rad = (float) ( angle*Math.PI/180);
-        float nouvX = (float)( Math.cos(2*rad+Math.PI/2)) ;
-        float nouvY = (float)( Math.sin(2*rad+3*Math.PI/2)-1) ;
+    public void angleSoleil( float Ze, float Az ){
+        if (Ze>=360 || Ze<=-360){ zenithSoleil=0;}  // Pas obligatoire
+        if (Az>=360|| Az<=-360){ azimutSoleil =0;}  // Pas obligatoire non plus
+        float radZe = (float) Math.toRadians(Ze);
+        float radAz = (float) Math.toRadians(Az);
+        float nouvX = (float)( Math.cos(radZe)*Math.cos(radAz));
+        float nouvY = (float)( Math.sin(radZe))-1;
+        float nouvZ = (float)( Math.cos(radZe)*Math.sin(radAz));
         SoleilGameItem.getPosition().x = nouvX;
         SoleilGameItem.getPosition().y = nouvY;
+        SoleilGameItem.getPosition().z = nouvZ;
+        AffichageZenith( Ze);
     }
+
+    public void AffichageZenith ( float Ze){
+        if (zenithSoleil >= -360 && zenithSoleil < -270) {
+            zenithSoleilpourAffichage = 360 + zenithSoleil;
+        } else if (zenithSoleil >= -270 && zenithSoleil < -90) {
+            zenithSoleilpourAffichage = -180 - zenithSoleil;
+        } else if (zenithSoleil >= -90 && zenithSoleil < 90) {
+            zenithSoleilpourAffichage =  zenithSoleil ;
+        }else if (zenithSoleil >= 90 && zenithSoleil < 270) {
+            zenithSoleilpourAffichage = 180-zenithSoleil;
+        }else if (zenithSoleil >= 270 && zenithSoleil < 360) {
+            zenithSoleilpourAffichage = zenithSoleil - 360  ;
+        }
+    }
+
 }
